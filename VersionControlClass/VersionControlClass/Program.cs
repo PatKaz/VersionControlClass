8d55bbab8d029b2c200a5ab79664864692803065
MichalRac <rac.michalmail@gmail.com>
2019-09-21 15:52:59 +0200

    Create simple calculator script

--- VersionControlClass/VersionControlClass/Program.cs
+++ VersionControlClass/VersionControlClass/Program.cs
@@ -10,6 +10,19 @@ namespace VersionControlClass
     {
         static void Main(string[] args)
         {
+            int a;
+            int b;
+
+            Console.WriteLine("Type first number: ");
+            a = System.Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type second number: ");
+            b = System.Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {a + b}");
+
+            Console.ReadKey();
+
         }
     }
 }
